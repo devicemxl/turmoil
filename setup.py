00d95000b27c77b5b35afa6a2930e630f3549669
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2023-03-10 23:50:21 -0600

    setup: packages path correction

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup(name='turmoil',
 version='0.0.1',
+packages=find_packages('src', exclude=['test']),
 description="Eliminates Pre-process and EDA's turmoil via Data Dictionary",
 url='https://github.com/devicemxl/turmoil/',
 author='David Ochoa',
 author_email='david@bajaanalytics.com',
 license='CC BY-NC-SA 4.0',
-packages=['turmoil'],
+#packages=['turmoil'],
 zip_safe=False)
