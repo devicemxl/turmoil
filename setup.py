bacb3c05641509f1f13ac2ba429214050f85bb9b
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2023-03-11 02:05:50 -0600

    Changes to be committed: 	modified:   setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='turmoil',
     version='0.0.1',
-    #packages=find_packages('src', exclude=['test']),
+    packages=find_packages('src', exclude=['test*']),
     #packages=['turmoil'],
     description="Eliminates Pre-process and EDA's turmoil via Data Dictionary",
     url='https://github.com/devicemxl/turmoil/',
@@ -11,15 +11,4 @@ setup(
     author_email='david@bajaanalytics.com',
     license='CC BY-NC-SA 4.0',
     zip_safe=False,
-    
-    install_requires=[
-        'requests',
-        'importlib-metadata; python_version == "3.8"',
-    ],
-    packages=find_packages(
-        # All keyword arguments below are optional:
-        where='src',  # '.' by default
-        include=['*'],  # ['*'] by default
-        exclude=['test*'],  # empty by default
-    ),
 )
