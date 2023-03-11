19e32596ff2eb14c9a7cc648efbb77bb0470c1ca
David  Ochoa Corrales <david.ochoa.corrales@gmail.com>
2023-03-10 21:56:55 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='turmoil',
+version='0.0.1',
+description="Eliminates Pre-process and EDA's turmoil via Data Dictionary",
+url='https://github.com/devicemxl/turmoil/',
+author='David Ochoa',
+author_email='david@bajaanalytics.com',
+license='CC BY-NC-SA 4.0',
+packages=['turmoil'],
+zip_safe=False)
